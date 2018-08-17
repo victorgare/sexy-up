a41d08eff5b527d7eb0eb4ed927fd479cab61646
Victor Rodrigues Garé Carnielli <garevictor@hotmail.com>
2018-08-16 21:53:45 -0300

    Solucao .NET MVC 5

---
+++ SexyUp/SexyUp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SexyUp.Web.MvcApplication" Language="C#" %>
--- sexy-up-website/src/components/buscar/buscar.js
+++ sexy-up-website/src/components/buscar/buscar.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react"
-// import logo from "./logotipo/googlelogo_color_272x92dp.png";
-// import logo from "../";
 
 class Buscar extends Component {
+
+    search() {
+        console.log("clicou");
+    }
+
     render() {
         return (
             <div>
-
                 <div className="row">
                     <div className="col-lg-8 offset-lg-2">
                         <div className="input-group">
                             <input type="text" className="form-control" />
                             <div className="input-group-append">
-                                <button className="btn btn-circle btn-primary">
+                                <button onClick={this.search} className="btn btn-circle btn-primary">
                                     <i className="fa fa-search" />
                                 </button>
                             </div>
