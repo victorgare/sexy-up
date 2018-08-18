c613a1d2c8432bf454a720dc2a3e9cfb21c60c5f
Victor Rodrigues Garé Carnielli <garevictor@hotmail.com>
2018-08-17 22:48:18 -0300

    Exclusao do projeto antio em react

--- sexy-up-website/src/index.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import '../src/vendor/fontawesome/css/font-awesome.min.css'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Root from './components/root';
-
-ReactDOM.render(<Root />, document.getElementById("root"));
