9277b67d052e2556c89801385e1d3d62124dbb85
Victor Rodrigues Garé Carnielli <garevictor@hotmail.com>
2018-08-23 22:36:54 -0300

    Injecao de dependencia

--- SexyUp/SexyUp.Web/ViewModels/IdentityModels.cs
+++ SexyUp/SexyUp.Web/ViewModels/IdentityModels.cs
@@ -20,7 +20,7 @@ namespace SexyUp.Web.ViewModels
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("Default", throwIfV1Schema: false)
         {
         }
 
