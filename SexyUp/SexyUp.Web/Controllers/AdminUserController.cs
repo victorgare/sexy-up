5e18c2ea1a1ad27b912acca61e3c5c5ed0f74c77
Victor Rodrigues Garé Carnielli <garevictor@hotmail.com>
2018-09-01 15:28:33 -0300

    Conclusao cadastro de administradores

--- SexyUp/SexyUp.Web/Controllers/AdminUserController.cs
+++ SexyUp/SexyUp.Web/Controllers/AdminUserController.cs
@@ -4,12 +4,14 @@ using SexyUp.Web.Controllers.Common;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using SexyUp.ApplicationCore.Entities;
 using SexyUp.Infrastructure.Context;
 using SexyUp.Web.Libraries.FlashMessage;
+using SexyUp.Web.ViewModels.AdminUser;
 
 namespace SexyUp.Web.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class AdminUserController : BaseAccountController
     {
 
@@ -26,16 +28,91 @@ namespace SexyUp.Web.Controllers
             }
         }
 
+        #region Register
+
+        [HttpGet]
         public ActionResult Register()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Register(AdminUserViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                FlashMessage.Error("Preencha todos os campos");
+                return View(nameof(Register), viewModel);
+            }
+
+            var user = new ApplicationUser
+            {
+                FirstName = viewModel.FirstName,
+                LastName = viewModel.LastName,
+                Email = viewModel.Email,
+                UserName = viewModel.Email
+            };
+
+            var result = UserManager.Create(user, "Admin@1234");
+            if (result.Succeeded)
+            {
+                UserManager.AddToRole(user.Id, "Administrador");
+
+                FlashMessage.Success("Criado com sucesso");
+
+                return RedirectToAction("Index", "AdminUser");
+            }
+
+            FlashMessage.Error(result.Errors.FirstOrDefault());
+            return View("Register", viewModel);
+        }
+
+        #endregion
+
+        #region Edit
+
         public ActionResult Edit(string id)
         {
             var user = UserManager.FindById(id);
-            return View();
+
+            var viewModel = new AdminUserViewModel
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email
+            };
+
+            return View(viewModel);
         }
+
+        [HttpPost]
+        public ActionResult Edit(AdminUserViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                FlashMessage.Error("Preencha todos os campos");
+                return View(nameof(Edit), viewModel);
+            }
+
+            var user = UserManager.FindById(viewModel.Id);
+
+            user.FirstName = viewModel.FirstName;
+            user.LastName = viewModel.LastName;
+            user.Email = viewModel.Email;
+            user.UserName = viewModel.Email;
+
+            var result = UserManager.Update(user);
+
+            if (result.Succeeded)
+            {
+                FlashMessage.Success("Alterado com sucesso");
+                return RedirectToAction(nameof(Index), "AdminUser");
+            }
+
+            FlashMessage.Error(result.Errors.FirstOrDefault());
+            return View(nameof(Edit), viewModel);
+        }
+        #endregion
 
         public ActionResult Lock(string id)
         {
---
+++ SexyUp/SexyUp.Web/ViewModels/AdminUser/AdminUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SexyUp.Web.ViewModels.AdminUser
+{
+    public class AdminUserViewModel
+    {
+        public string Id { get; set; }
+
+        [Display(Name = "Nome"), Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Sobrenome"), Required]
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress), Required]
+        public string Email { get; set; }
+    }
+}
