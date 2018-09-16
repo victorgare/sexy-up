ef01564cbef3d3886e705a2b5f81a4aff7961468
Peterson de Cerqueira Matos <RM73784@fiap.com.br>
2018-09-16 11:21:36 -0300

    Revert "Exibição do resultado da busca"
    This reverts commit c2bbd4e62e3e42f710c41e5293f900f05b21a7ac.

--- SexyUp/SexyUp.Web/Vendor/bootstrap-select/js/bootstrap-select.js
+++ SexyUp/SexyUp.Web/Vendor/bootstrap-select/js/bootstrap-select.js
@@ -490,7 +490,7 @@
 
   // part of this is duplicated in i18n/defaults-en_US.js. Make sure to update both.
   Selectpicker.DEFAULTS = {
-    noneSelectedText: 'Selecione',
+    noneSelectedText: 'Nothing selected',
     noneResultsText: 'No results matched {0}',
     countSelectedText: function (numSelected, numTotal) {
       return (numSelected == 1) ? "{0} item selected" : "{0} items selected";
