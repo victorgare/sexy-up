739eb596be7e64c46fa89d84bd13791d0eb568a2
Victor Rodrigues Garé Carnielli <garevictor@hotmail.com>
2018-09-23 00:57:46 -0300

    Inicio da funcionalidade de lojas próximas

--- SexyUp/SexyUp.Web/Scripts/front.js
+++ SexyUp/SexyUp.Web/Scripts/front.js
@@ -44,7 +44,7 @@ $(function () {
     // from the video
     if (screenWidth > 800) {
         $('video.bg-video').attr('autoplay', true);
-        $('video.bg-video').each(function(){
+        $('video.bg-video').each(function () {
             this.play();
         });
     }
@@ -56,7 +56,7 @@ $(function () {
 
     if ($('#countdown').length > 0) {
 
-        $('#countdown').countdown('2020/10/10', function(event) {
+        $('#countdown').countdown('2020/10/10', function (event) {
             $(this).html(event.strftime(
                 '<div class="col-6 col-sm-3"><div class="display-3 text-shadow">%D</div>days</div>'
                 + '<div class="col-6 col-sm-3"><div class="display-3 text-shadow">%H</div>hours</div>'
@@ -220,133 +220,132 @@ $(function () {
     // ------------------------------------------------------- //
     // Google Maps
     // ------------------------------------------------------ //
-    if ($('#map').length > 0) {
+    //if ($('#map').length > 0) {
 
 
-        function initMap() {
+    //function initMap() {
 
-            var location = new google.maps.LatLng(50.0875726, 14.4189987);
+    //    var location = new google.maps.LatLng(50.0875726, 14.4189987);
 
-            var mapCanvas = document.getElementById('map');
-            var mapOptions = {
-                center: location,
-                zoom: 16,
-                panControl: false,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            }
-            var map = new google.maps.Map(mapCanvas, mapOptions);
+    //    var mapCanvas = document.getElementById('map');
+    //    var mapOptions = {
+    //        center: location,
+    //        zoom: 16,
+    //        panControl: false,
+    //        mapTypeId: google.maps.MapTypeId.ROADMAP
+    //    }
+    //    var map = new google.maps.Map(mapCanvas, mapOptions);
 
-            var markerImage = 'img/marker.png';
+    //    var markerImage = 'img/marker.png';
 
-            var marker = new google.maps.Marker({
-                position: location,
-                map: map,
-                icon: markerImage
-            });
+    //    var marker = new google.maps.Marker({
+    //        position: location,
+    //        map: map,
+    //        icon: markerImage
+    //    });
 
-            var contentString = '<div class="info-window">' +
-                '<h3>Info Window Content</h3>' +
-                '<div class="info-content">' +
-                '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
-                '</div>' +
-                '</div>';
+    //    var contentString = '<div class="info-window">' +
+    //        '<h3>Info Window Content</h3>' +
+    //        '<div class="info-content">' +
+    //        '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+    //        '</div>' +
+    //        '</div>';
 
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString,
-                maxWidth: 400
-            });
+    //    var infowindow = new google.maps.InfoWindow({
+    //        content: contentString,
+    //        maxWidth: 400
+    //    });
 
-            marker.addListener('click', function () {
-                infowindow.open(map, marker);
-            });
+    //    marker.addListener('click', function () {
+    //        infowindow.open(map, marker);
+    //    });
 
-            var styles = [{
-                "featureType": "landscape",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "lightness": 65
-                }, {
-                    "visibility": "on"
-                }]
-            }, {
-                "featureType": "poi",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "lightness": 51
-                }, {
-                    "visibility": "simplified"
-                }]
-            }, {
-                "featureType": "road.highway",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "visibility": "simplified"
-                }]
-            }, {
-                "featureType": "road.arterial",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "lightness": 30
-                }, {
-                    "visibility": "on"
-                }]
-            }, {
-                "featureType": "road.local",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "lightness": 40
-                }, {
-                    "visibility": "on"
-                }]
-            }, {
-                "featureType": "transit",
-                "stylers": [{
-                    "saturation": -100
-                }, {
-                    "visibility": "simplified"
-                }]
-            }, {
-                "featureType": "administrative.province",
-                "stylers": [{
-                    "visibility": "off"
-                }]
-            }, {
-                "featureType": "water",
-                "elementType": "labels",
-                "stylers": [{
-                    "visibility": "on"
-                }, {
-                    "lightness": -25
-                }, {
-                    "saturation": -100
-                }]
-            }, {
-                "featureType": "water",
-                "elementType": "geometry",
-                "stylers": [{
-                    "hue": "#ffff00"
-                }, {
-                    "lightness": -25
-                }, {
-                    "saturation": -97
-                }]
-            }];
+    //    var styles = [{
+    //        "featureType": "landscape",
+    //        "stylers": [{
+    //            "saturation": -100
+    //        }, {
+    //            "lightness": 65
+    //        }, {
+    //            "visibility": "on"
+    //        }]
+    //    }, {
+    //        "featureType": "poi",
+    //        "stylers": [{
+    //            "saturation": -100
+    //        }, {
+    //            "lightness": 51
+    //        }, {
+    //            "visibility": "simplified"
+    //        }]
+    //    }, {
+    //        "featureType": "road.highway",
+    //        "stylers": [{
+    //            "saturation": -100
+    //        }, {
+    //            "visibility": "simplified"
+    //        }]
+    //    }, {
+    //        "featureType": "road.arterial",
+    //        "stylers": [{
+    //            "saturation": -100
+    //        }, {
+    //            "lightness": 30
+    //        }, {
+    //            "visibility": "on"
+    //        }]
+    //    }, {
+    //        "featureType": "road.local",
+    //        "stylers": [{
+    //            "saturation": -100
+    //        }, {
+    //            "lightness": 40
+    //        }, {
+    //            "visibility": "on"
+    //        }]
+    //    }, {
+    //        "featureType": "transit",
+    //        "stylers": [{
+    //            "saturation": -100
+    //        }, {
+    //            "visibility": "simplified"
+    //        }]
+    //    }, {
+    //        "featureType": "administrative.province",
+    //        "stylers": [{
+    //            "visibility": "off"
+    //        }]
+    //    }, {
+    //        "featureType": "water",
+    //        "elementType": "labels",
+    //        "stylers": [{
+    //            "visibility": "on"
+    //        }, {
+    //            "lightness": -25
+    //        }, {
+    //            "saturation": -100
+    //        }]
+    //    }, {
+    //        "featureType": "water",
+    //        "elementType": "geometry",
+    //        "stylers": [{
+    //            "hue": "#ffff00"
+    //        }, {
+    //            "lightness": -25
+    //        }, {
+    //            "saturation": -97
+    //        }]
+    //    }];
 
-            map.set('styles', styles);
-
-
-        }
-
-        google.maps.event.addDomListener(window, 'load', initMap);
+    //    map.set('styles', styles);
 
 
-    }
+    //}
 
+    //google.maps.event.addDomListener(window, 'load', initMap);
+
+    //}
+    
 });
 
 // ------------------------------------------------------ //
